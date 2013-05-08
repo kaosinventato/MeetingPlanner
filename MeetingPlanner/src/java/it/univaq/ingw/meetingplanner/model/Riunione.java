e71d9223000b8f5b91b892ac89136411412ca0e1
Daniele Nasuti <daniele.nasuti@gmail.com>
2013-05-08 18:32:24 +0200

    Inserite le interfacce per gestire le entità

---
+++ MeetingPlanner/src/java/it/univaq/ingw/meetingplanner/model/Riunione.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.univaq.ingw.meetingplanner.model;
+
+/**
+ *
+ * @author Daniele
+ */
+public interface Riunione {
+    
+}
